96dd27e12c29c28b33ba6f76998a2e0edbab7f3f
Kishan <kishangupta21995@gmail.com>
2019-12-24 17:49:04 -0500

    Initial commit

---
+++ app/src/main/java/com/app/myyoutube/MainActivity.java
@@ -0,0 +1,32 @@
+package com.app.myyoutube;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        YouTubePlayerView youTubePlayerView=findViewById(R.id.youtube_player);
+        getLifecycle().addObserver(youTubePlayerView);
+        youTubePlayerView.addYouTubePlayerListener(new AbstractYouTubePlayerListener() {
+            @Override
+            public void onReady(YouTubePlayer youTubePlayer) {
+                /*super.onReady(youTubePlayer);*/
+
+                String videoId="9RfrVFuYxOA";
+                youTubePlayer.loadVideo(videoId,0);
+
+            }
+        });
+
+    }
+}
